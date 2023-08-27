6e6b5a08ef28e9880922c7546e008bda181fd4c6
Shounak <shounsach@gmail.com>
2023-08-27 13:11:34 +0530

    Implemented a base64 encoder decoder chat bot for discord

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-use discord_flows::{
-    model::Message,
-    ProvidedBot, Bot,
-};
+use base64::{engine::general_purpose, Engine as _};
+use discord_flows::{model::Message, Bot, ProvidedBot};
 use flowsnet_platform_sdk::logger;
 
 #[no_mangle]
@@ -9,13 +7,16 @@ use flowsnet_platform_sdk::logger;
 pub async fn run() -> anyhow::Result<()> {
     let discord_token = std::env::var("discord_token").unwrap();
     let bot = ProvidedBot::new(discord_token);
-    bot.listen(|msg| handler(&bot, msg)).await;
+    bot.listen(|msg: Message| handler(&bot, msg)).await;
     Ok(())
 }
 
 async fn handler(bot: &ProvidedBot, msg: Message) {
     logger::init();
     let discord = bot.get_client();
+    let binding = msg.content.trim().to_lowercase();
+    let message: Vec<_> = binding.split(' ').collect();
+    let mut resp;
 
     if msg.author.bot {
         log::debug!("ignored bot message");
@@ -26,13 +27,45 @@ async fn handler(bot: &ProvidedBot, msg: Message) {
         return;
     }
 
-    let channel_id = msg.channel_id;
-    let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg.content);
+    match message[0] {
+        "" => resp = format!("Empty input I shall do nothing"),
+        "encode" => {
+            let string = message[1..].join(" ");
+            let encoded = general_purpose::STANDARD_NO_PAD.encode(&string);
+            resp = format!(
+                "Your original string was {string} \n Your base64 encoded string is {encoded} \n"
+            )
+        }
+        "decode" => {
+            let string = message[1..].join(" ");
+            let decoded = general_purpose::STANDARD_NO_PAD.decode(&string).unwrap();
 
-    _ = discord.send_message(
-        channel_id.into(),
-        &serde_json::json!({
-            "content": resp
-        }),
-    ).await;
+            match std::str::from_utf8(&decoded) {
+                Ok(decoded_string) => {
+                    resp = format!(
+                    "Your original string was {string} \n Your base64 decoded string is {decoded_string} \n"
+                )
+                }
+                Err(_) => resp = format!("Something went wrong while decoding"),
+            };
+        }
+        _ => {
+            resp = format!("Invalid method only encode and decode are accepted methods the case does not matter");
+        }
+    }
+
+    if message.len() < 2 {
+        resp = format!("Invalid input")
+    }
+
+    let channel_id = msg.channel_id;
+
+    _ = discord
+        .send_message(
+            channel_id.into(),
+            &serde_json::json!({
+                "content": resp
+            }),
+        )
+        .await;
 }
